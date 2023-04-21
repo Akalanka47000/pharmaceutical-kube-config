9f533262ae9b61208db99545e53b9079be5d1d48
Akalanka <akalankaperera128@gmail.com>
2023-04-21 22:56:50 +0530

    Added argocd manifests to version control

--- scripts/seal-secrets.sh
+++ scripts/seal-secrets.sh
@@ -4,3 +4,6 @@ do
     cat $folder/secrets.yml | kubeseal --format yaml --controller-namespace kube-system --controller-name sealed-secrets > $folder/sealed-secrets.yml
     echo "Sealed $(basename "$folder") secrets successfully"
 done
+
+cat argocd/secrets.yml | kubeseal --format yaml --controller-namespace kube-system --controller-name sealed-secrets > argocd/sealed-secrets.yml
+echo "Sealed argocd secrets successfully"
